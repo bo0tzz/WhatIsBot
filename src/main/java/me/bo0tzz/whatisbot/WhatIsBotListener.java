aabfa4786cc2b5e9c70224785497d94da2d861ae
bo0tzz <btdewilligen@gmail.com>
2016-02-07 18:21:44 +0100

    Remove URL from message

--- src/main/java/me/bo0tzz/whatisbot/WhatIsBotListener.java
+++ src/main/java/me/bo0tzz/whatisbot/WhatIsBotListener.java
@@ -54,7 +54,6 @@ public class WhatIsBotListener implements Listener {
             results.add(InlineQueryResultArticle.builder()
                     .title(title)
                     .messageText(description)
-                    .url(url)
                     .build()
             );
         }
